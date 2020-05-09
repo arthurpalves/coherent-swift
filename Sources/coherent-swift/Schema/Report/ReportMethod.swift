6a40a8fc648259eecb0d5e0b67db67a42c548f74
Arthur Alves <arthuralves.p@gmail.com>
2020-05-09 22:12:32 +0200

    feat: exclude contentString from reports

--- Sources/coherent-swift/Schema/Report/ReportMethod.swift
+++ Sources/coherent-swift/Schema/Report/ReportMethod.swift
@@ -9,7 +9,7 @@ import Foundation
 public struct ReportMethod: Codable {
     let name: String
     var cohesion: String
-    var contentString: String
+    var contentString: String = ""
     var properties: [ReportProperty]
     
     init(name: String, cohesion: String = "", contentString: String = "", properties: [ReportProperty] = []) {
@@ -17,5 +17,23 @@ public struct ReportMethod: Codable {
         self.cohesion = cohesion
         self.contentString = contentString
         self.properties = properties
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case name, cohesion, properties
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        cohesion = try container.decode(String.self, forKey: .cohesion)
+        properties = try container.decode([ReportProperty].self, forKey: .properties)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(cohesion, forKey: .cohesion)
+        try container.encode(properties, forKey: .properties)
     }
 }
