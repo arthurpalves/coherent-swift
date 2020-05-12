5397e627fb6d796943696155bfda7c9872e84a49
Arthur Alves <arthuralves.p@gmail.com>
2020-05-12 21:19:35 +0200

    chore: ability to generate json to use as shields.io badge

--- Sources/coherent-swift/Helpers/IO/ReportGenerator.swift
+++ Sources/coherent-swift/Helpers/IO/ReportGenerator.swift
@@ -10,14 +10,17 @@ import PathKit
 
 public protocol ReportGenerator {
     var reports_file: String { get }
+    var badge_file: String { get set }
     var reports_path: String { get set }
     
     func addToReport(file: String, cohesion: String, meetsThreshold: Bool, definitions: [ReportDefinition], to report: ReportOutput) -> ReportOutput
     func generateReport(_ report: ReportOutput) -> (Bool, Path?)
+    func generateBadge(_ report: ReportOutput) -> (Bool, Path?)
 }
 
 extension ReportGenerator {
     public var reports_file: String { "coherent-swift.json" }
+    var badge_file: String { "coherent-badge.json" }
     var reports_path: String { "/tmp/coherent-swift/" }
     
     public func addToReport(file: String, cohesion: String, meetsThreshold: Bool, definitions: [ReportDefinition], to report: ReportOutput) -> ReportOutput {
@@ -32,7 +35,24 @@ extension ReportGenerator {
         
         var reportCopy = report
         reportCopy.report_date = Date().logTimestamp()
+        
+        do {
+            try Task.run(bash: "mkdir -p \(reports_path)")
+            generateBadge(reportCopy)
+            return write(reportCopy, toFile: path)
+        } catch {
+            return (false, nil)
+        }
 
+    }
+    
+    public func generateBadge(_ report: ReportOutput) -> (Bool, Path?) {
+        let path = Path("\(reports_path)/\(badge_file)")
+        let coherentBadge = CoherentBadge(schemaVersion: 1, label: "cohesion", message: report.cohesion, color: "blue")
+        return write(coherentBadge, toFile: path)
+    }
+    
+    private func write<T>(_ encodableObject: T, toFile file: Path) -> (Bool, Path?) where T : Encodable {
         let encoder = JSONEncoder()
         if #available(OSX 10.15, *) {
             encoder.outputFormatting = [.prettyPrinted, .sortedKeys, .withoutEscapingSlashes]
@@ -41,14 +61,14 @@ extension ReportGenerator {
         }
         
         do {
-            try Task.run(bash: "mkdir -p \(reports_path)")
-            try Task.run(bash: "touch \(path.absolute().description)")
+            try Task.run(bash: "touch \(file.absolute().description)")
             
-            let encoded = try encoder.encode(reportCopy)
+            let encoded = try encoder.encode(encodableObject)
             guard let encodedJSONString = String(data: encoded, encoding: .utf8) else { return (false, nil) }
-            try encodedJSONString.write(toFile: path.absolute().description, atomically: true, encoding: .utf8)
+            try encodedJSONString.write(toFile: file.absolute().description, atomically: true, encoding: .utf8)
             
-            return (true, path)
+            return (true, file)
+            
         } catch {
             return (false, nil)
         }
