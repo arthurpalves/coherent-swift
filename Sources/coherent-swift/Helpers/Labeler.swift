bdbda27a1dcdbb0a68cee89ebe4a49bc4832aa55
Arthur Alves <arthuralves.p@gmail.com>
2020-05-25 12:54:18 +0200

    chore: simple helper to return a method's type

---
+++ Sources/coherent-swift/Helpers/Labeler.swift
@@ -0,0 +1,28 @@
+//
+//  coherent-swift
+//
+//  Created by Arthur Alves on 25/05/2020.
+//
+
+import Foundation
+
+class Labeler {
+    static let shared = Labeler()
+    
+    func methodType(_ name: String, lineContent: String) -> MethodType {
+        let methodsCleanName = Cleaner.shared.cleanMethodName(name)
+        var methodType: MethodType = .publicMethod
+        let lineItems = lineContent
+            .replacingOccurrences(of: "\t", with: "")
+            .split(separator: " ")
+        
+        for item in lineItems {
+            if methodsCleanName.contains(item) { break }
+            if let type = MethodType(rawValue: String(item)) {
+                methodType = type
+                break
+            }
+        }
+        return methodType
+    }
+}
