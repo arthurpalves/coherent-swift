eef86118ad7c59e55dc6d21fcc8ef9f7ba43c5f2
Arthur Alves <arthuralves.p@gmail.com>
2020-11-18 11:23:53 +0100

    ref: correct flag and inject 'UserInputHelper'

--- Sources/CoherentSwift/Commands/Initializer.swift
+++ Sources/CoherentSwift/Commands/Initializer.swift
@@ -16,14 +16,14 @@ struct Initializer: ParsableCommand {
     var verbose = false
     
     @Flag(name: [.long, .customShort("t")], help: "Show timestamps.")
-    var showTimestamp = false
+    var showTimestamps = false
     
     func run() throws {
-        let logger = Logger(verbose: verbose, showTimestamp: showTimestamp)
-        logger.logSection("$ ", item: "coherent-swift init", color: .purple)
+        let logger = Logger(verbose: verbose, showTimestamp: showTimestamps)
+        logger.logSection("$ coherent-swift init", item: "", color: .purple)
         
         let path = try TemplateDirectory().path
-        let specHelper = SpecHelper(logger: logger)
+        let specHelper = SpecHelper(logger: logger, userInputHelper: UserInputHelper(logger: logger))
         try specHelper.generate(from: path)
         logger.logInfo(item: " ")
     }
