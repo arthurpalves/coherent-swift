92086c710af00a9f5d0226fe1c95cec87d117306
Arthur Alves <arthuralves.p@gmail.com>
2020-11-18 12:34:51 +0100

    feat: include PathKit extension to safeJoin two paths

---
+++ Sources/CoherentSwiftCore/Extensions/PathKit+SafeJoin.swift
@@ -0,0 +1,31 @@
+//
+//  CoherentSwift
+//
+
+import PathKit
+
+extension Path {
+  func safeJoin(path: Path) throws -> Path {
+    let newPath = self + path
+
+    if !newPath.absolute().description.hasPrefix(absolute().description) {
+      throw SuspiciousFileOperation(basePath: self, path: newPath)
+    }
+
+    return newPath
+  }
+}
+
+class SuspiciousFileOperation: Error {
+  let basePath: Path
+  let path: Path
+
+  init(basePath: Path, path: Path) {
+    self.basePath = basePath
+    self.path = path
+  }
+
+  var description: String {
+    return "Path `\(path)` is located outside of base path `\(basePath)`"
+  }
+}
