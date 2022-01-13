9507fd81cc72ad2dccb53b431006c46042c7e241
Arthur Alves <arthuralves.p@gmail.com>
2022-01-13 16:34:44 +0100

    chore: bump to version 0.5.11

--- Sources/CoherentSwift/main.swift
+++ Sources/CoherentSwift/main.swift
@@ -8,7 +8,7 @@ struct CoherentSwift: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "coherent-swift",
         abstract: "A command-line tool to analyze and report Swift code cohesion",
-        version: "0.5.10",
+        version: "0.5.11",
         subcommands: [
             Initializer.self,
             Report.self
